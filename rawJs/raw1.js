391676b8669a0f362b3a0f2478d384f5452f87e4
shahin2525 <shahin451999@gmail.com>
2025-01-09 09:23:31 -0800

    practice data type

---
+++ rawJs/raw1.js
@@ -0,0 +1,14 @@
+// variable data type
+const age = 20;
+const name = "arif";
+const property = undefined;
+const isMarried = false;
+const car = null;
+const degree = ["ssc", "hss", "bsc"];
+const familyMembers = {
+  parents: true,
+  sisters: true,
+  howManySisters: 3,
+};
+
+console.log(typeof degree);
